ec2b004c59a2b7f346ba9637436b18dc37d29c47
schauppi <46676413+schauppi@users.noreply.github.com>
2024-03-09 19:44:07 +0100

    configured and implemented logging with info and error messages

--- src/autogen_config/GetConfig.py
+++ src/autogen_config/GetConfig.py
@@ -1,11 +1,24 @@
-import os
-from dotenv import load_dotenv
+from src.utils.logging.log_config import setup_logging
 import autogen
 from autogen import config_list_from_json
+import logging
+from dotenv import load_dotenv
+
+import os
+
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
 
 dotenv_path = os.path.normpath(os.path.join(
     os.path.dirname(__file__), '..', '..', '.env'))
-load_dotenv(dotenv_path=dotenv_path)
+
+try:
+    load_dotenv(dotenv_path=dotenv_path)
+    logger.info("Environment variables loaded successfully.")
+except Exception as e:
+    logger.error("Failed to load the .env file.", exc_info=e)
 
 
 class GetConfig:
@@ -17,7 +30,10 @@ class GetConfig:
         """
         Initialize with API key and config list.
         """
+        logger.info('Initializing GetConfig class')
         self.api_key = os.environ.get('OPENAI_API_KEY', '')
+        if not self.api_key:
+            logger.error('OPENAI_API_KEY not found in environment variables')
         self.config_list = self.load_and_enrich_config_list()
 
     @property
@@ -25,7 +41,9 @@ class GetConfig:
         """
         Returns the base directory path.
         """
-        return os.path.dirname(os.path.dirname(__file__))
+        base_dir = os.path.dirname(os.path.dirname(__file__))
+        logger.info(f'Retrieved base directory path: {base_dir}')
+        return base_dir
 
     def load_and_enrich_config_list(self):
         """
@@ -33,12 +51,17 @@ class GetConfig:
         """
         config_path = os.path.join(
             os.path.dirname(__file__), "OAI_CONFIG_LIST")
-
-        config_list = config_list_from_json(
-            env_or_file=config_path,
-            filter_dict={"model": "gpt-4-turbo-preview"}
-        )
-        for config in config_list:
-            config['api_key'] = self.api_key
-
+        try:
+            config_list = config_list_from_json(
+                env_or_file=config_path,
+                filter_dict={"model": "gpt-4-turbo-preview"}
+            )
+            logger.info('Config list loaded successfully')
+            for config in config_list:
+                config['api_key'] = self.api_key
+            logger.info('Config list enriched with API key')
+        except Exception as e:
+            logger.error(
+                "Failed to load or enrich the config list.", exc_info=e)
+            config_list = []
         return {'config_list': config_list}
