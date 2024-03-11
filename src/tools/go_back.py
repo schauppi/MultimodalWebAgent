bb04d1e5d36096d020f2eacb925dbe0f5412db35
schauppi <46676413+schauppi@users.noreply.github.com>
2024-03-11 10:14:21 +0100

    implement tools

---
+++ src/tools/go_back.py
@@ -0,0 +1,34 @@
+from src.webdriver.webdriver import WebDriver
+from src.configs.logging.logging_config import setup_logging
+
+import logging
+import time
+
+setup_logging()
+logger = logging.getLogger()
+
+
+def go_back() -> str:
+    """
+    Navigates back one page in the browser history using a WebDriver instance, and returns a response string with the result of the action.
+
+    Args: 
+        None
+
+    Returns:
+        str: A response string stating the success of the action and the current URL.
+    """
+
+    try:
+        webdriver = WebDriver.getInstance()
+        driver = webdriver.getDriver()
+
+        logger.info("Navigating back 1 page...")
+        driver.go_back()
+
+        time.sleep(3)
+
+        return "Success. Went back 1 page. Current URL is: " + driver.url
+    except Exception as e:
+        logger.error("An error occurred while navigating back: %s", str(e))
+        return "Error occurred while navigating back. Please check the logs for more details."
