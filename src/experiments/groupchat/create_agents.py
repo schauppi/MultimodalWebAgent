2d6d9e8554384e10db40fc175c129dde4ec0fae0
schauppi <46676413+schauppi@users.noreply.github.com>
2024-03-13 18:28:52 +0100

    qa agent definition

---
+++ src/experiments/groupchat/create_agents.py
@@ -0,0 +1,10 @@
+from src.create_oai_agent.oai_agent_creator import OAIAssistantCreator
+
+import os
+
+dotenv_path = os.path.normpath(os.path.join(
+    os.path.dirname(__file__), '..', '..', '.env'))
+
+config_path = 'src/experiments/groupchat/utils/qa_tool_definition.json'
+instruction_path = 'src/experiments/groupchat/utils/qa_instructions.json'
+assistant_save_path = "src/data/assistant_id.json"
