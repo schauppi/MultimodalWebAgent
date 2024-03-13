9bae67f6eef4b9cb51c4398a6746bd625f3fe1c5
schauppi <46676413+schauppi@users.noreply.github.com>
2024-03-13 18:42:53 +0100

    Update create_agents.py

--- src/experiments/groupchat/create_agents.py
+++ src/experiments/groupchat/create_agents.py
@@ -8,3 +8,7 @@ dotenv_path = os.path.normpath(os.path.join(
 config_path = 'src/experiments/groupchat/utils/qa_tool_definition.json'
 instruction_path = 'src/experiments/groupchat/utils/qa_instructions.json'
 assistant_save_path = "src/data/assistant_id.json"
+
+creator = OAIAssistantCreator(
+    dotenv_path, config_path, instruction_path, assistant_save_path)
+creator.run()
