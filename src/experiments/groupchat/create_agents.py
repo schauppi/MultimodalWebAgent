c203e074624c8db357f984c0a71c8a62b8473cc5
schauppi <46676413+schauppi@users.noreply.github.com>
2024-03-13 18:50:00 +0100

    create qa agent

--- src/experiments/groupchat/create_agents.py
+++ src/experiments/groupchat/create_agents.py
@@ -3,12 +3,12 @@ from src.create_oai_agent.oai_agent_creator import OAIAssistantCreator
 import os
 
 dotenv_path = os.path.normpath(os.path.join(
-    os.path.dirname(__file__), '..', '..', '.env'))
+    os.path.dirname(__file__), '..', '..', '..', '.env'))
 
 config_path = 'src/experiments/groupchat/utils/qa_tool_definition.json'
 instruction_path = 'src/experiments/groupchat/utils/qa_instructions.json'
 assistant_save_path = "src/data/assistant_id.json"
 
 creator = OAIAssistantCreator(
-    dotenv_path, config_path, instruction_path, assistant_save_path)
+    dotenv_path, config_path, instruction_path, assistant_save_path, "QualityAssuranceAgent")
 creator.run()
