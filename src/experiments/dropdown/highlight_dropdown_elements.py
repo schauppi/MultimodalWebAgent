ebbeef5acacccfcee32c9c60f4ec4068d120bcfb
schauppi <46676413+schauppi@users.noreply.github.com>
2024-04-02 14:52:12 +0200

    dropdown experiments

---
+++ src/experiments/dropdown/highlight_dropdown_elements.py
@@ -0,0 +1,89 @@
+from src.webdriver.webdriver import WebDriver
+from typing import Literal
+import json
+import logging
+
+from src.configs.logging.logging_config import setup_logging
+setup_logging()
+logger = logging.getLogger()
+
+
+def format_description(elements: list) -> tuple:
+    """
+    Formats the description of page elements and their bounding box coordinates.
+
+    Args:
+        elements (list): A list of element dictionaries with keys like 'ariaLabel', 'x', 'y', 'text'.
+
+    Returns:
+        tuple: A tuple containing a JSON string of elements' descriptions and a list of bounding box coordinates.
+    """
+    labels = []
+    bbox_coordinates = []
+    for i, bbox in enumerate(elements):
+        text = bbox.get("ariaLabel") or ""
+        bbox_coordinates.append((bbox.get("x"), bbox.get("y")))
+        if not text.strip():
+            text = bbox.get("text")
+        labels.append({str(i):  text})
+
+    bbox_descriptions = json.dumps(labels, indent=4)
+    return bbox_descriptions, bbox_coordinates
+
+
+def highlight_elements(driver, mark: Literal["click", "input", "all", "remove", "dropdown"]):
+    """
+    Highlights elements on a webpage based on the mark type.
+
+    Args:
+        driver (WebDriver): An instance of the WebDriver.
+        mark (Literal["click", "input", "all", "remove"]): The type of elements to mark.
+
+    Returns:
+        A tuple containing descriptions and coordinates of marked elements or the driver itself after unmarking elements.
+        In case of errors, it returns an error message.
+    """
+    if mark not in ["click", "input", "all", "remove", "dropdown"]:
+        raise ValueError(
+            "Mark must be either 'click', 'input', 'all', 'remove' or dropdown.")
+
+    try:
+        with open('src/tools/utils/mark_page.js') as f:
+            mark_page_script = f.read()
+
+        if mark == "click":
+            elements = driver.evaluate(f"""() => {{
+                {mark_page_script}
+                return markClickableElements();
+            }}""")
+            bbox_descriptions, bbox_coordinates = format_description(elements)
+            return bbox_descriptions, bbox_coordinates, driver
+        elif mark == "input":
+            elements = driver.evaluate(f"""() => {{
+                {mark_page_script}
+                return markInputElements();
+            }}""")
+            bbox_descriptions, bbox_coordinates = format_description(elements)
+            return bbox_descriptions, bbox_coordinates, driver
+        elif mark == "all":
+            elements = driver.evaluate(f"""() => {{
+                {mark_page_script}
+                return markAllElements();
+            }}""")
+            bbox_descriptions, bbox_coordinates = format_description(elements)
+            return bbox_descriptions, bbox_coordinates, driver
+        elif mark == "dropdown":
+            elements = driver.evaluate(f"""() => {{
+                {mark_page_script}
+                return markDropdownElements();
+            }}""")
+            bbox_descriptions, bbox_coordinates = format_description(elements)
+            return bbox_descriptions, bbox_coordinates, driver
+        else:
+            driver.evaluate(f"""() => {{
+                {mark_page_script}
+                return unmarkPage();
+            }}""")
+            return driver
+    except Exception as e:
+        return str(e)
--- src/experiments/dropdown/test.py
+++ src/experiments/dropdown/test.py
@@ -1,24 +1,27 @@
 
 from src.tools.read_url import read_url
 from src.tools.click_element import click_element
-from src.tools.utils.highlight_elements import highlight_elements
 from src.tools.utils.get_webdriver_instance import get_webdriver_instance
 from src.tools.utils.get_b64_screenshot import get_b64_screenshot
+from src.experiments.dropdown.highlight_dropdown_elements import highlight_elements
 
+import json
 import time
 
-url = "https://www.globalsqa.com/demo-site/select-dropdown-menu/"
+url = "https://www.amazon.de/JustSun-Trousers-Casual-Elastic-Drawstring/dp/B0CG1L9M5T/ref=sr_1_3_sspa?crid=28BQ4759MI9WD&dib=eyJ2IjoiMSJ9.cnQ2pFKcqrLzn4gPOR0XVq0elcgJLPMmpxIyc06mjyRgxRE3Y9W_6HVtk0p4q_HCUbo82lV7XrzPgZUTtA8xLNiUpqP_Lgx6M7ueAPNHYN-O8qDdXLIuOSGsIaDV09T-xOqqS7PNGDeAViTdq3RIFUho6JNb_pbDMErbg9y-m7x8TInfB3_8mP5n1EEj8xlxLRhS9EgyYvRmL4WiTfnhZUK9qvs6F6xSUrsEWT13VBuX0reekBG1II2SfBFKrO-RnarSG6KuP-_uZbiKyuqvTelT6HAh7R8TkSMquOaQNmo.kS3zVowhYSNXPnN0d7MRUO0CMNzktie-p_eQcS2P5lQ&dib_tag=se&keywords=trousers&qid=1712058349&sprefix=trouser%2Caps%2C140&sr=8-3-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
 
 read_url(url)
 
-time.sleep(5)
-
-click_element("click on the dropdown menu to select the langauge")
-
-time.sleep(5)
+time.sleep(10)
 
 driver = get_webdriver_instance()
 
-driver.keyboard.type("Austria")
+with open("src/experiments/dropdown/mark_page_dropdown.js") as f:
+    driver.evaluate(f.read())
 
-time.sleep(5)
+rects = driver.evaluate("MultimodalWebSurfer.getInteractiveRects();")
+
+with open("src/experiments/dropdown/data/rects.json", "w") as f:
+    f.write(json.dumps(rects))
+
+
