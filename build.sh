e73105a66c1d4a2ddb2d0c309dc5f419ba315dff
abhinav <abhinava.sanyal16@gmail.com>
2024-05-14 11:27:10 +0530

    WIP: fix broken dependecies

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 # Install Poetry
@@ -6,9 +5,6 @@ curl -sSL https://install.python-poetry.org | python3 -
 
 # Add Poetry to PATH
 export PATH="$HOME/.local/bin:$PATH"
-
-# Install SQLite dependencies
-apt-get update && apt-get install -y sqlite3 libsqlite3-dev
 
 # Install dependencies using Poetry
 poetry install
