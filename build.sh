04709c88045d92b842681d14407c8715066286ad
abhinav <abhinava.sanyal16@gmail.com>
2024-05-14 11:21:25 +0530

    WIP: added sqlite in build script stage

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 # Install Poetry
@@ -5,6 +6,9 @@ curl -sSL https://install.python-poetry.org | python3 -
 
 # Add Poetry to PATH
 export PATH="$HOME/.local/bin:$PATH"
+
+# Install SQLite dependencies
+apt-get update && apt-get install -y sqlite3 libsqlite3-dev
 
 # Install dependencies using Poetry
 poetry install
